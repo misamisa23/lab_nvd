fec62e795f56cea870175b2a216c0247ee78fc75
misamisa23 <misaa23@gmail.com>
2020-01-04 22:40:27 +0100

    lab 5 zad 3

--- lab05/zad2/js/app.js
+++ lab05/zad2/js/app.js
@@ -1,70 +1,95 @@
 const products = [
-	{id: 1,title: 'LenovoThinkpad x280', price: 1000.00, qty: 1, image: './img/ThinkPad_x280.png'},  
-	{id: 2,title: 'Apple Macbook Pro',price: 2500.00, qty: 1,image: './img/MacBook-Pro.png'},  
-	{id: 3,title: 'Amazon Kindle Ebook',price: 150.00,qty: 1,image: './img/Amazon_Kindle.png'},  
-	{id: 4,title: 'USB-C to HDMI cable',price: 10, qty: 1, image: './img/usbC_to_hdmi.jpg'},  
+  {
+    id: 1,
+    title: "LenovoThinkpad x280",
+    price: 1000.0,
+    qty: 1,
+    image: "./img/ThinkPad_x280.png"
+  },
+  {
+    id: 2,
+    title: "Apple Macbook Pro",
+    price: 2500.0,
+    qty: 1,
+    image: "./img/MacBook-Pro.png"
+  },
+  {
+    id: 3,
+    title: "Amazon Kindle Ebook",
+    price: 150.0,
+    qty: 1,
+    image: "./img/Amazon_Kindle.png"
+  },
+  {
+    id: 4,
+    title: "USB-C to HDMI cable",
+    price: 10,
+    qty: 1,
+    image: "./img/usbC_to_hdmi.jpg"
+  }
 ];
-
 
 // Your Code goes here
 
-
-Vue.component('shopping-cart', {
-	data () {
-		return {
-			
-		}
-	},
-	props: ["items"]
+Vue.component("shopping-cart", {
+  data() {
+    return {};
+  },
+  props: ["items"]
 });
 
-var app = new Vue({
-	el: '#app',
-    data: {
-		cartItems: [],
-		products: products
-    },
-    methods: {
-     addToCart: function(title, price, qty) {
-		var item = {
-			title: title,
-			price: price,
-			qty: qty,
-			itemTotal: function() {
-				return this.price * this.qty
-			}
-		};
-		if (this.cartItems.filter(i => i.title === title).length > 0){
-
-			for (i = 0; i < this.cartItems.length; i++) {
-				if(this.cartItems[i].title == title){
-					this.cartItems[i].qty += qty;
-				}
-			  }
-
-		}else{
-			this.cartItems.push(item);
-		}
-		return
-	 },
-	 removeItem: function(index) {
-			this.cartItems.splice(index,1);
-	 },
-
-    
+Vue.component("product", {
+	data() {
+	  return {};
 	},
-	computed: {
-		// a computed getter
-		Total: function () {
-		  // `this` points to the vm instance
-		  return this.cartItems.map(item => item.itemTotal()).reduce((prev, next) => prev + next, 0)
-		}
-	  },
-	  filters: {
-		formatCurrency: function (value) {
-		  if (!value) return ''
-		 
-		  return value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-		}
-	  }
-  })
+	props: ["prods"]
+  });
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    cartItems: [],
+    products: products
+  },
+  methods: {
+    addToCart: function(title, price, qty) {
+      var item = {
+        title: title,
+        price: price,
+        qty: qty,
+        itemTotal: function() {
+          return this.price * this.qty;
+        }
+      };
+      if (this.cartItems.filter(i => i.title === title).length > 0) {
+        for (i = 0; i < this.cartItems.length; i++) {
+          if (this.cartItems[i].title == title) {
+            this.cartItems[i].qty += qty;
+          }
+        }
+      } else {
+        this.cartItems.push(item);
+      }
+      return;
+    },
+    removeItem: function(index) {
+      this.cartItems.splice(index, 1);
+    }
+  },
+  computed: {
+    // a computed getter
+    Total: function() {
+      // `this` points to the vm instance
+      return this.cartItems
+        .map(item => item.itemTotal())
+        .reduce((prev, next) => prev + next, 0);
+    }
+  },
+  filters: {
+    formatCurrency: function(value) {
+      if (!value) return "";
+
+      return value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    }
+  }
+});
